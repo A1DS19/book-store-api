27c194032c053e434d736858f8a263732a27d968
Jose Enrique Padilla <jp06171@gmail.com>
2024-09-15 18:54:22 -0600

    rocket initial config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn hello() -> String {
+    format!("Hello, world!")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/api", routes![hello])
 }
