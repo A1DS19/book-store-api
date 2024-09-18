5ca20e4e909ee4078bec6656b93cebaa2d4c5675
Jose Enrique Padilla <jp06171@gmail.com>
2024-09-17 19:03:50 -0600

    new state

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
 mod config;
+mod dto;
 mod entities;
+mod fairings;
+mod guards;
 mod migrator;
+mod routes;
 
 use config::db_connect;
+use fairings::cors::CORS;
 use migrator::Migrator;
+use rocket::http::Status;
+use routes::{auth, authors, books};
+use routes::{Response, SuccessResponse};
 use sea_orm_migration::MigratorTrait;
 use std::process;
 
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn hello() -> String {
-    format!("Hello, world!")
+#[get("/health_check")]
+fn health_check() -> Response<&'static str> {
+    Ok(SuccessResponse((Status::Ok, "Healthy")))
 }
 
 #[launch]
 async fn rocket() -> _ {
+    dotenvy::dotenv().ok();
+
+    let config = match config::app_config::AppConfig::new() {
+        Ok(config) => config,
+        Err(e) => {
+            eprintln!("Failed to load configuration: {}", e);
+            process::exit(1);
+        }
+    };
+
     let db_conn = match db_connect().await {
         Ok(conn) => conn,
         Err(_) => {
@@ -33,6 +51,29 @@ async fn rocket() -> _ {
     };
 
     rocket::build()
+        .attach(CORS)
         .manage(db_conn)
-        .mount("/api", routes![hello])
+        .manage(config)
+        .mount("/api", routes![health_check])
+        .mount("/api/auth", routes![auth::sign_in, auth::sign_up, auth::me])
+        .mount(
+            "/api/authors",
+            routes![
+                authors::index,
+                authors::create,
+                authors::show,
+                authors::update,
+                authors::delete
+            ],
+        )
+        .mount(
+            "/api/books",
+            routes![
+                books::index,
+                books::create,
+                books::show,
+                books::update,
+                books::delete
+            ],
+        )
 }
